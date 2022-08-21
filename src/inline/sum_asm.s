5c3550eaf01ef33078e84b9ce6e88e9348918caa
kakts <kakts.git@gmail.com>
2022-08-21 18:46:13 +0900

    8.2: fix sum_asm.s

--- src/inline/sum_asm.s
+++ src/inline/sum_asm.s
@@ -1,11 +1,11 @@
     .global sum_asm
-    .text ; .textセクション開始
+    .text # .textセクション開始
 sum_asm:
-    movl $0, %eax ; EAX <- 0
-    movl $array, %edx ; EDX <- 配列arrayのアドレス
-    movl array_size, %ecx ; ECX <- 配列の大きさ
+    movl $0, %eax
+    movl $array, %edx # EDX <- 配列arrayのアドレス
+    movl array_size, %ecx # ECX <- 配列の大きさ
 L1:
-    add(%edx), %eax ; EAXに配列の要素を加算
-    add $4, %edx ＃配列のアドレスを+4(intは32ビットなので)
-    loop L1 ; ECXを-1し、ゼロでなければL1にジャンプ
-    ret ; 計算結果はレジスタEAXに格納されている
+    add (%edx), %eax # EAXに配列の要素を加算
+    add $4, %edx # 配列のアドレスを+4(intは32ビットなので)
+    loop L1 # ECXを-1し、ゼロでなければL1にジャンプ
+    ret # 計算結果はレジスタEAXに格納されている
